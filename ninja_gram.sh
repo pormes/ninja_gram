2a02c713b76c149e5c5be3bb809f597fc5966622
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-06 10:24:16 +0700

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -15,18 +15,35 @@ echo ""
 echo ""
 
 pkg install -y && pkg update -y && pkg upgrade -y
+pkg install unzip -y
+unzip ninja_gram.zip
+cd ninja_gram
 pkg install python2 -y
 pkg install python -y
-pip install --upgrade pip setuptools
+pip install --upgrade pip setuptools whell numpy pillow
+pip install -r requirements.txt
 pkg install clang -y
 pkg install curl -y
 pkg install apache2 -y
 pkg install nodejs-lts -y
-python -m pip install --upgrade pip wheel numpy pillow setuptools
+python -m pip install --upgrade pip wheel numpy pillow django setuptools 
 python setup.py install --user
 pip install -U instabot
 pip install clang
+pip install selenium
+pip install twine
+pip install mechanize
+pip install requests
 pip install tox-travis codecov
+pip install python-instagram
+pip install django-social-auth
+pip install py-oauth2
+pip install instabot-py
+pip install instabot
+pip install django_social_followers
+pip install wfdb
+pip install prometheus-client
+pip install django-smart-proxy
 pip install InstagramGraphQL
 pip install instagram-api
 pip install instagram-oauth
@@ -35,13 +52,14 @@ pip install instagram_private_api
 pip install git +https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 curl -O https://nodejs.org/dist/v12.8.1-1/SHASUMS256.txt.sig
+npm i instabot
 npm i http-proxy-agent
 npm i https-proxy-agent
 npm i socks-proxy-agent
 npm i agent-base
+npm i instagram-web-api
 npm i instagram-private-api
 npm install
-
 echo "NEXT COMMAND FOR EXECUTION"
 echo "$ unzip ninja_gram.zip"
 echo "$ cd ninja"
