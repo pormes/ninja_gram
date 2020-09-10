65fc378480a288bfd52a389f3da5d0efac75fe58
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-11 04:02:08 +0700

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -18,9 +18,108 @@ pkg install -y && pkg update -y && pkg upgrade -y
 pkg install unzip -y
 unzip ninja_gram.zip
 cd ninja_gram
+pkg install git-crypt -y
+pkg install git-delta -y
+pkg install git-lfs -y
+pkg install git -y
+pkg install gitea -y
+pkg install go-findimagedupes -y
+pkg install goaccess -y
+pkg install golang -y
+pkg install google-glog -y
+pkg install googletest -y
+pkg install gotty -y
+pkg install gst-plugins-bad -y
+pkg install gst-plugins-base -y
+pkg install gst-plugins-good -y
+pkg install gst-plugins-ugly -y
+pkg install hashdeep -y
+pkg install html2text -y
+pkg install htop -y
+pkg install httping -y
+pkg install httrack -y
+pkg install imagemagick -y
+pkg install imgflo -y
+pkg install imlib2 -y
+pkg install indent -y
+pkg install inetutils -y
+pkg install inotify-tools -y
+pkg install ipcalc -y
+pkg install iperf3 -y
+pkg install ipfs -y
+pkg install ipmitool -y
+pkg install json-c -y
+pkg install json-glib -y
+pkg install jsoncpp -y
+pkg install keychain -y
+pkg install keystone -y
+pkg install lastpass-cli -y
+pkg install lazyread -y
+pkg install ldc -y
+pkg install ldns -y
+pkg install leveldb -y
+pkg install proot-distro -y
+pkg install proot -y
+pkg install proxmark3 -y
+pkg install proxychains-ng -y
+pkg install qemu-system-x86_64-headless -y
+pkg install qpdf -y
+pkg install quickjs -y
+pkg install radare2 -y
+pkg install seccure -y
+pkg install shell2http -y
+pkg install shellharden -y
+pkg install shellinabox -y
+pkg install sleuthkit -y
+pkg install socat -y
+pkg install solidity -y
+pkg install sox -y
+pkg install speexdsp -y
+pkg install spiped -y
+pkg install sqlcipher -y
+pkg install sqlmap -y
+pkg install squid -y
+pkg install sshpass -y
+pkg install strace -y
+pkg install streamripper -y
+pkg install stunnel -y
+pkg install subversion -y
+pkg install termux-am -y
+pkg install termux-api -y
+pkg install termux-apt-repo -y
+pkg install termux-auth -y
+pkg install termux-create-package -y
+pkg install termux-elf-cleaner -y
+pkg install termux-exec -y
+pkg install termux-keyring -y
+pkg install termux-licenses -y
+pkg install termux-services -y
+pkg install termux-tools -y
+pkg install tigervnc -y
+pkg install timewarrior -y
+pkg install tin-summer -y
+pkg install tintin++ -y
+pkg install tinyfugue -y
+pkg install tinyproxy -y
+pkg install tinyscheme -y
+pkg install tor -y
+pkg install torsocks -y
+pkg install tracepath -y
+pkg install translate-shell -y
+pkg install transmission -y
+pkg install tsocks -y
+pkg install xcb-proto -y
+pkg install xdelta3 -y
+pkg install xmake -y
+pkg install xmlsec -y
+pkg install xmlstarlet -y
+pkg install xorg-util-macros -y
+pkg install xorgproto -y
+pkg install xorriso -y
+pkg install xtrans -y
 pkg install python2 -y
 pkg install python -y
-pip install --upgrade pip whell numpy pillow setuptools
+pip install --upgrade pip wheel numpy pillow setuptools
 pkg install clang -y
 pkg install curl -y
 pkg install apache2 -y
@@ -38,7 +137,10 @@ pip install python-instagram
 pip install django-social-auth
 pip install py-oauth2
 pip install instabot-py
-pip install instabot
+pip install pytest-seleniumbase
+pip install CProxy
+pip install signalfx-tracing
+pip install sqlite-utils
 pip install django_social_followers
 pip install wfdb
 pip install prometheus-client
@@ -48,7 +150,7 @@ pip install instagram-api
 pip install instagram-oauth
 pip install instagram-sdk
 pip install instagram_private_api
-pip install git +https://git@github.com/ping/instagram_private_api.git@1.6.0
+pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 curl -O https://nodejs.org/dist/v12.8.1-1/SHASUMS256.txt.sig
 npm i instabot
@@ -59,6 +161,7 @@ npm i agent-base
 npm i instagram-web-api
 npm i instagram-private-api
 npm install
+npm-upgrade
 echo "NEXT COMMAND FOR EXECUTION"
 echo "$ cd ninja"
 echo "$ python ninja_gram.py"
