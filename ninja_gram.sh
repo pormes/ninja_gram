d5c8541dbb6ae1c69c779e3b253a141fe71b1734
Pormes <66345108+pormes@users.noreply.github.com>
2020-12-19 09:40:00 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -21,6 +21,7 @@ apt install unzip -y
 unzip ninja_gram.zip
 python3 -m pip install --upgrade pip wheel numpy pillow setuptools 
 python3 setup.py install --user
+pip install bootstrap -v 5.0.0.alpha3
 pip install -U instabot
 pip install clang
 pip install selenium
@@ -60,6 +61,8 @@ pip install -r requirements.txt
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 curl -O https://nodejs.org/dist/v12.8.1-1/SHASUMS256.txt.sig
+npm install -g npm
+npm install npm@latest
 npm i instabot
 npm i http-proxy-agent
 npm i https-proxy-agent
@@ -67,6 +70,8 @@ npm i socks-proxy-agent
 npm i agent-base
 npm i instagram-web-api
 npm i instagram-private-api
+npm i bootstrap@5.0.0-alpha1
+npm install bootstrap@next
 npm install
 echo ""
 echo "DON'T FORGET TO SUBSCRIBE PORMES17"
