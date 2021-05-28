c9b112143c5aac13baf3f08ec9a79e41f45f9cd0
Pormes <66345108+pormes@users.noreply.github.com>
2021-05-28 15:52:21 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -21,60 +21,13 @@ apt install unzip -y
 unzip ninja_gram.zip
 python3 -m pip install --upgrade pip wheel numpy pillow setuptools 
 python3 setup.py install --user
-pip install bootstrap -v 5.0.0.alpha3
 pip install -U instabot
-pip install clang
-pip install selenium
-pip install colorama
-pip install twine
-pip install mechanize
-pip install pyramid
-pip install falcon
-pip install eve
-pip install CherryPy
-pip install tornado
-pip install tox-travis codecov
-pip install python-instagram
-pip install django-social-auth
-pip install py-oauth2
-pip install CProxy
-pip install signalfx-tracing
-pip install sqlite-utils
-pip install django_social_followers
-pip install prometheus-client
-pip install django-smart-proxy
-pip install InstagramGraphQL
-pip install instagram-api
-pip install instagram-oauth
-pip install instagram-sdk
 pip install instagram_private_api
-pip install configparser
-pip install Flask
-pip install Flask-RESTful
-pip install redis
-pip install rq
-pip install marshmallow
-pip install flake8
-pip install Flask-Sessionstore
-pip install Flask-SQLAlchemy
-pip install flask-talisman
-pip install pysaml2
-pip install flask-Login
-pip install Flask-Cors
-pip install Flask-SeaSurf
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
-curl -O https://nodejs.org/dist/v12.8.1-1/SHASUMS256.txt.sig
+curl -O https://nodejs.org/dist/v14.15.4/SHASUMS256.txt.sig
 npm install -g npm
 npm install npm@latest
-npm i instabot
-npm i http-proxy-agent
-npm i https-proxy-agent
-npm i socks-proxy-agent
-npm i agent-base
-npm i instagram-web-api
-npm i instagram-private-api
-npm i bootstrap@5.0.0-alpha1
 npm install bootstrap@next
 npm install
 echo ""
@@ -86,3 +39,4 @@ echo "SUPPORT ME AND SUBSCRIBE PORMES17"
 echo "THANKS FOR SUPPORT ME"
 echo "CODED BY PORMES17"
 exit
+
