8d6514a4ae9e23e045122575565964c7b9266d3b
Pormes <66345108+pormes@users.noreply.github.com>
2021-05-28 21:20:11 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -18,6 +18,7 @@ apt install -y && apt update -y && apt upgrade -y
 apt install apache2 -y
 apt install nodejs -y
 apt install unzip -y
+apt install python -y
 unzip ninja_gram.zip
 python3 -m pip install --upgrade pip wheel numpy pillow setuptools 
 python3 setup.py install --user
@@ -26,9 +27,6 @@ pip install instagram_private_api
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 curl -O https://nodejs.org/dist/v14.15.4/SHASUMS256.txt.sig
-npm install -g npm
-npm install npm@latest
-npm install bootstrap@next
 npm install
 echo ""
 echo "DON'T FORGET TO SUBSCRIBE PORMES17"
