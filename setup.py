88efc6564cb9429a1ef50ee3bdd823f4f94d73f1
Pormes <66345108+pormes@users.noreply.github.com>
2021-06-06 07:18:15 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,61 +1,122 @@
-from codecs import open
-from os import path
+#!/usr/bin/env python
+# This file is protected via CODEOWNERS
 
-from setuptools import find_packages, setup
+import os
+import re
+import sys
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup
 
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write(
+        """
+==========================
+Unsupported Python version
+==========================
+This version of urllib3 requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+This may be because you are using a version of pip that doesn't
+understand the python_requires classifier. Make sure you
+have pip >= 9.0 and setuptools >= 24.2, then try again:
+    $ python -m pip install --upgrade pip setuptools
+    $ python -m pip install urllib3
+This will install the latest version of urllib3 which works on your
+version of Python. If you can't upgrade your pip (or Python), request
+an older version of urllib3:
+    $ python -m pip install "urllib3<2"
+""".format(
+            *(REQUIRED_PYTHON + CURRENT_PYTHON)
+        )
+    )
+    sys.exit(1)
+
+
+base_path = os.path.dirname(__file__)
+
+# Get the version (borrowed from SQLAlchemy)
+with open(os.path.join(base_path, "src", "urllib3", "_version.py")) as fp:
+    VERSION = (
+        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S).match(fp.read()).group(1)
+    )
+
+
+with open("README.rst", encoding="utf-8") as fp:
+    # Remove reST raw directive from README as they're not allowed on PyPI
+    # Those blocks start with a newline and continue until the next newline
+    mode = None
+    lines = []
+    for line in fp:
+        if line.startswith(".. raw::"):
+            mode = "ignore_nl"
+        elif line == "\n":
+            mode = "wait_nl" if mode == "ignore_nl" else None
+
+        if mode is None:
+            lines.append(line)
+    readme = "".join(lines)
+
+with open("CHANGES.rst", encoding="utf-8") as fp:
+    changes = fp.read()
+
+version = VERSION
 
 setup(
-    name="instabot",
-    version="0.117.0",
-    description="Instagram bot scripts for promotion and API python wrapper.",
-    long_description=long_description,
-    author="pormes ",
-    author_email="anonymous17111990@gmail.com",
-    license="Apache Software License 2.0",
-    url="https://github.com/pormes/ninja_gram",
-    keywords=["instagram", "bot", "api", "wrapper"],
-    install_requires=[
-        "certifi>=2019.11.28",
-        "chardet>=3.0.4",
-        "future>=0.18.2",
-        "huepy>=1.2.1",
-        "idna>=2.8",
-        "pysocks>=1.7.1",
-        "pytz>=2019.3",
-        "requests>=2.22.0",
-        "requests-toolbelt>=0.9.1",
-        "responses>=0.10.9",
-        "schedule>=0.6.0",
-        "six>=1.14.0",
-        "tqdm>=4.41.1",
-        "urllib3>=1.25.7",
-        "mock>=3.0.5",
-        "moviepy>=1.0.1",
-        "Pillow>=6.2.2",
-        "pytest>=4.6.9",
-        "pycryptodome>=3.9.7",
-    ],
+    name="urllib3",
+    version=version,
+    description="HTTP library with thread-safe connection pooling, file post, and more.",
+    long_description="\n\n".join([readme, changes]),
+    long_description_content_type="text/x-rst",
     classifiers=[
-        # How mature is this project? Common values are
-        "Development Status :: 5 - Production/Stable",
-        # Indicate who your project is intended for
-        "Intended Audience :: Information Technology",
-        # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: Apache Software License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
     ],
-    packages=find_packages(),
+    keywords="urllib httplib threadsafe filepost http https ssl pooling",
+    author="Andrey Petrov",
+    author_email="andrey.petrov@shazow.net",
+    url="https://urllib3.readthedocs.io/",
+    project_urls={
+        "Documentation": "https://urllib3.readthedocs.io/",
+        "Code": "https://github.com/urllib3/urllib3",
+        "Issue tracker": "https://github.com/urllib3/urllib3/issues",
+    },
+    license="MIT",
+    packages=[
+        "urllib3",
+        "urllib3.contrib",
+        "urllib3.contrib._securetransport",
+        "urllib3.util",
+    ],
+    package_dir={"": "src"},
+    requires=[],
+    python_requires=">=3.6, <4",
+    extras_require={
+        "brotli": [
+            "brotli>=1.0.9; platform_python_implementation == 'CPython'",
+            "brotlicffi>=0.8.0; platform_python_implementation != 'CPython'",
+        ],
+        "secure": [
+            "pyOpenSSL>=0.14",
+            "cryptography>=1.3.4",
+            "idna>=2.0.0",
+            "certifi",
+        ],
+        "socks": ["PySocks>=1.5.6,<2.0,!=1.5.7"],
+    },
 )
